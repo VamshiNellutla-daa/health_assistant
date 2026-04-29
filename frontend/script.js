e7fc1ea70baf54d8acdd7037564585892b7a1ad3
VamshiNellutla-daa <nvkrish2022@gmail.com>
2026-04-29 16:17:07 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -153,7 +153,7 @@ async function showResults() {
             formData.append('image', imageInput.files[0]);
         }
 
-        const response = await fetch('https://health-assistant-backend.onrender.com/api/analyze', {
+        const response = await fetch('https://health-assistant-backend.onrender.com/analyze', {
             method: 'POST',
            // headers: { 'Content-Type': 'application/json' },
            // body: JSON.stringify(data)
