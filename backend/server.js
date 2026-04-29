5d6ee6cc227202b9b65cef3fcbfc6804e2b66bb6
VamshiNellutla-daa <nvkrish2022@gmail.com>
2026-04-29 16:31:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: 'https://health-assistant-3qrn.onrender.com', 
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../frontend')));
